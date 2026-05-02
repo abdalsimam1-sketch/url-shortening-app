5b33d94a22e5c41a1d5b95171bdb1962fb477de6
abdalsimam1-sketch <abdalsimam1@gmail.com>
2026-05-02 08:11:09 +0100

    add : mock data for reusablility

---
+++ src/data/mockData.js
@@ -0,0 +1,22 @@
+import brand from "../assets/images/icon-brand-recognition.svg";
+import records from "../assets/images/icon-detailed-records.svg";
+import customizable from "../assets/images/icon-fully-customizable.svg";
+
+export const mockData = [
+  {
+    img: brand,
+    name: "Brand Recongnition",
+    text: "Boost your brand recognition with each click. Genrric links dont mean a thing. Branded links help instill confidence in your content",
+  },
+  {
+    img: records,
+    name: "Detailed Records",
+    text: "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions",
+  },
+  ,
+  {
+    img: customizable,
+    name: "Fully Customizable",
+    text: "Imporve brand awareness and content discoverability through customizable links, supercharging audience engagement",
+  },
+];
